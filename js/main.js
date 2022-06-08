f6b00e556e8624d1783ce405d30ade611da8c575
Alex King <calicoalix@gmail.com>
2022-06-08 16:00:31 +1000

    init grid traveler

--- js/main.js
+++ js/main.js
@@ -17,3 +17,16 @@ const fib = (n, memo = {}) => {
 }
 
 console.log(fib(800))
+
+
+
+console.log('GRID TRAVELER ==================');
+
+
+const gridTraveler = (m, n) => {
+    if (m === 1 && n === 1) return 1;
+    if (m === 0 || n === 0) return 0;
+    return gridTraveler(m - 1, n) + gridTraveler(m, n - 1)
+}
+
+console.log(gridTraveler(1, 1))
