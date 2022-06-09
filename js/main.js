3a32964195b826db584214e77451bc29037376b6
Alex King <calicoalix@gmail.com>
2022-06-09 14:53:49 +1000

    memoized howSum O(n*m^2)

--- js/main.js
+++ js/main.js
@@ -60,7 +60,7 @@ console.log('CAN SUM =======================');
 //     return false;
 // }
 
-// Memoization
+// Memoization [ O(m*n) ]
 const canSum = (targetSum, numbers, memo = {}) => {
     if (targetSum in memo) return memo[targetSum];
     if (targetSum === 0) return true;
@@ -79,3 +79,42 @@ const canSum = (targetSum, numbers, memo = {}) => {
 }
 
 console.log(canSum(300, [7, 14]))
+
+
+console.log('HOW SUM =======================');
+
+// Brute Force [ O(n^m * m) ]
+// const howSum = (targetSum, numbers) => {
+//     if (targetSum === 0) return [];
+//     if (targetSum < 0) return null;
+    
+//     for (let num of numbers){
+//         const remainder = targetSum - num;
+//         const remainderResult = howSum(remainder, numbers)
+//         if (remainderResult !== null){
+//             return [...remainderResult, num];
+//         }
+//     }
+//     return null
+// }
+
+// Memoized [ O(n*m^2) ]
+const howSum = (targetSum, numbers, memo = {}) => {
+    if (targetSum in memo) return memo[targetSum];
+    if (targetSum === 0) return [];
+    if (targetSum < 0) return null;
+    
+    for (let num of numbers){
+        const remainder = targetSum - num;
+        const remainderResult = howSum(remainder, numbers, memo)
+        if (remainderResult !== null){
+            memo[targetSum] = [...remainderResult, num];
+            return memo[targetSum];
+        }
+    }
+    memo[targetSum] = null
+    return null
+}
+
+console.log(howSum(7, [5, 3, 4, 7]));
+console.log(howSum(300, [7, 14]));
