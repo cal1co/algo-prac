61810fa0ed51556309ddb0b7a0cfd612c52964d3
Alex King <calicoalix@gmail.com>
2022-06-09 14:11:55 +1000

    canSum memoized

--- js/main.js
+++ js/main.js
@@ -61,19 +61,21 @@ console.log('CAN SUM =======================');
 // }
 
 // Memoization
-const canSum = (targetSum, numbers) => {
+const canSum = (targetSum, numbers, memo = {}) => {
+    if (targetSum in memo) return memo[targetSum];
     if (targetSum === 0) return true;
     if (targetSum < 0) return false;
 
     for (let num of numbers){
         // console.log(num);
         const remainder = targetSum - num;
-        if (canSum(remainder, numbers) === true){
+        if (canSum(remainder, numbers, memo) === true){
+            memo[targetSum] = true;
             return true;
         } 
     }
-
+    memo[targetSum] = false;
     return false;
 }
 
-console.log(canSum(7, [2, 3]))
+console.log(canSum(300, [7, 14]))
