b10326e0b4459dfac70ed799a05f1ab819720bc6
Alex King <calicoalix@gmail.com>
2022-06-10 14:54:29 +1000

    canConstruct memoized: O(n*m^2)

--- js/main.js
+++ js/main.js
@@ -173,3 +173,41 @@ console.log(bestSum(100, [1, 2, 5, 25]));
 
 
 console.log('CAN CONSTRUCT =======================');
+
+// Brute Force [ O(n^m) ]
+// const canConstruct = (target, wordBank) => {
+//     if (target === ''){
+//         return true;
+//     }
+//     for (let word of wordBank){
+//         if (target.indexOf(word) === 0){
+//             const suffix = target.slice(word.length);
+    
+//             if (canConstruct(suffix, wordBank) === true){
+//                 return true
+//             }
+//         }
+//     }
+// }
+
+// Memoized [ O(n * m^2) ]
+const canConstruct = (target, wordBank, memo = {}) => {
+    if (target in memo) return memo[target];
+    if (target === ''){
+        return true;
+    }
+    for (let word of wordBank){
+        if (target.indexOf(word) === 0){
+            const suffix = target.slice(word.length);
+    
+            if (canConstruct(suffix, wordBank, memo) === true){
+                memo[target] = true;
+                return true
+            }
+        }
+    }
+    memo[target] = false
+    return false;
+}
+
+console.log(canConstruct('eeeeeeeeeeeeeeeeeeeeeeeeef', ['e', 'ee', 'eee', 'eeee','eeeee','eeeeee']))
