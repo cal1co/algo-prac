3eccb9d43bffbcedf1fdc1b9a6a45f3d461b1b5e
Alex King <calicoalix@gmail.com>
2022-06-10 11:57:13 +1000

    init can construct

--- js/main.js
+++ js/main.js
@@ -170,3 +170,6 @@ const bestSum = (targetSum, numbers, memo = {}) => {
 console.log(bestSum(7, [5, 3, 4, 7]));
 console.log(bestSum(100, [1, 2, 5, 25]));
 
+
+
+console.log('CAN CONSTRUCT =======================');
