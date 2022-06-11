5ab6b8c99949c331b1bfcc09b355eaf7fd427ee4
Alex King <calicoalix@gmail.com>
2022-06-12 00:03:48 +1000

    allConstruct O(n^m)

--- js/main.js
+++ js/main.js
@@ -255,19 +255,42 @@ console.log(countConstruct("eeeeeeeeeeeeeeeeeeeeeef", ["e", "ee","eee",'eeee', "
 console.log('ALL CONSTRUCT =======================');
 
 // Brute Force
-const allConstruct = (target, wordBank) => {
+// const allConstruct = (target, wordBank) => {
+//     if (target === '') return [[]];
+
+//     const result = [];
+
+//     for (let word of wordBank){
+//         if (target.indexOf(word) === 0){
+//             const suffix = target.slice(word.length);
+//             const suffixWays = allConstruct(suffix, wordBank);
+//             const targetWays = suffixWays.map(way => [ word, ...way ])
+//             result.push(...targetWays)
+//         }
+//     }
+//     return result
+// }
+
+// ALL CONSTRUCT: O(n^m)
+const allConstruct = (target, wordBank, memo = {}) => {
+    if (target in memo) return memo[target]
     if (target === '') return [[]];
 
-    let output = []
+    const result = [];
 
     for (let word of wordBank){
         if (target.indexOf(word) === 0){
-            allConstruct(target.slice(word.length), wordBank)
+            const suffix = target.slice(word.length);
+            const suffixWays = allConstruct(suffix, wordBank, memo);
+            const targetWays = suffixWays.map(way => [ word, ...way ])
+            result.push(...targetWays)
         }
     }
-    return []
+    memo[target] = result;
+    return result
 }
-
+console.log(allConstruct('purple', ['purp', 'p', 'ur', 'le', 'purpl']))
 console.log(allConstruct('abcdef', ['ab', 'abc', 'cd', 'def', 'efg']))
+console.log(allConstruct("eeeeeeeeeeeeeeeeeeeeeef", ["e", "ee","eee",'eeee', "eeeee", "eeeeee"]));
 // console.log(allConstruct('hello', ['cat', 'dog', 'mouse']))
 // console.log(allConstruct('', ['cat', 'dog', 'mouse']))
